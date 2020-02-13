ed934359ab0b526861d41e2ff02203f3bf0c3700
Cristian Gaggino <cristian.gaggino@mercadolibre.com>
2020-02-13 16:20:25 -0300

    * Reverted some features

--- px-checkout/src/main/java/com/mercadopago/android/px/internal/features/payment_result/mappers/PaymentResultBodyModelMapper.java
+++ px-checkout/src/main/java/com/mercadopago/android/px/internal/features/payment_result/mappers/PaymentResultBodyModelMapper.java
@@ -2,7 +2,6 @@ package com.mercadopago.android.px.internal.features.payment_result.mappers;
 
 import android.support.annotation.NonNull;
 import com.mercadopago.android.px.configuration.PaymentResultScreenConfiguration;
-import com.mercadopago.android.px.internal.features.business_result.BusinessPaymentResultTracker;
 import com.mercadopago.android.px.internal.features.business_result.PaymentRewardMapper;
 import com.mercadopago.android.px.internal.view.PaymentResultBody;
 import com.mercadopago.android.px.internal.view.PaymentResultMethod;
@@ -31,8 +30,7 @@ public class PaymentResultBodyModelMapper extends Mapper<PaymentModel, PaymentRe
 
         return new PaymentResultBody.Model.Builder()
             .setMethodModels(methodModels)
-            .setRewardViewModel(new PaymentRewardMapper(new BusinessPaymentResultTracker())
-                .map(model.getPaymentReward()))
+            .setRewardViewModel(new PaymentRewardMapper().map(model.getPaymentReward()))
             .setReceiptId(String.valueOf(paymentResult.getPaymentId()))
             .setTopFragment(configuration.getTopFragment())
             .setBottomFragment(configuration.getBottomFragment())
